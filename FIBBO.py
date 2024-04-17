6f9f06aab8bd9bdf810d10e566eb3508c056c458
Justin Chen <103361304+Chenny0892@users.noreply.github.com>
2024-04-18 09:53:06 +1000

    Initial commit

---
+++ FIBBO.py
@@ -0,0 +1,14 @@
+
+def fibbonaci(n):  
+    fib_seq = [1,1]
+
+
+    for i in range(2,n):
+        next = fib_seq[-1] +  fib_seq[-2]  
+        fib_seq.append(next)  
+    return fib_seq 
+
+n = int(input("input a pos int"))
+
+print(fibbonaci(n))  
+
